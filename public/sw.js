172063092d2b6db6ca37b13c10bb3f01c23741e5
porky375 <hiscutt.tom@gmail.com>
2026-07-03 12:44:00 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "porky-antivirus-v1";
+
+const urlsToCache = [
+  "/",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
