81a4feb75f893fc99aea6081dffdd2c35ab44c85
porky375 <hiscutt.tom@gmail.com>
2026-07-03 13:44:09 +0100

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>🛡 Porky Antivirus</h1>
+
+            <p>
+                Real-Time Protection Active
+            </p>
+        </header>
+    );
+}
